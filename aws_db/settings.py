755e672544b2d86195d793d448454e79bc093e76
Sumit Mishra <sumitmishra@Sumits-MacBook-Air.local>
2025-06-08 12:57:43 +0530

    deployement setting update

--- aws_db/settings.py
+++ aws_db/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6$mw(vtu4muzu7j2j8ft5gkl9jrjo5sjaze*!b=2uvb&yd0bu('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
